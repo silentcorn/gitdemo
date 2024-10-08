245d2fbef369819f4e47f75d1a69330d28857402
silentcorn <kevingorostieta123@gmail.com>
2024-10-07 19:55:30 -0500

    First commit

---
+++ collatz.py
@@ -0,0 +1,19 @@
+#collatz conjecture
+
+
+flag = 0
+n = int(input('whats n? '))
+if n == 1:
+    print(True)
+else:
+    while n != 1:
+        if n % 2 == 0:
+            n = n/2
+            flag += 1
+            continue
+        elif n % 2 != 0:
+            n = (3 * n) + 1
+            flag += 1
+            continue
+
+print(flag)
